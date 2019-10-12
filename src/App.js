4b46089f12b6a42521caab0548a7f139c8b0b63a
steffenpedersen <steffen.pedersen@live.dk>
2019-10-12 13:34:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import PokemonList from './PokemonList';
+import axios from 'axios';
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Make a request for a Pokémon's name
+    axios.get(currentPageUrl).then(res => {
+      setLoading(false)
+
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+  }, [currentPageUrl])
+
+  if (loading) return "Loading..."
+
+  return (
+    <PokemonList pokemon={pokemon}/>
+  );
+}
+
+export default App;
