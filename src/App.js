5516688fdd5a32b048ac41d36631026ea2eccab2
steffenpedersen <steffen.pedersen@live.dk>
2019-10-12 14:21:20 +0200

    Add pagination to pokemon list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import PokemonList from './PokemonList';
+import Pagination from './Pagination';
 import axios from 'axios';
 
 function App() {
@@ -12,21 +13,41 @@ function App() {
 
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+  useEffect(() => { 
+    setLoading(true)
+    let cancel
     // Make a request for a Pokémon's name
-    axios.get(currentPageUrl).then(res => {
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
       setLoading(false)
 
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
+
+    return () => cancel()
   }, [currentPageUrl])
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function gotoPrevPage() {
+    setCurrentPageUrl(prevPageUrl)
+  }
 
   if (loading) return "Loading..."
 
   return (
-    <PokemonList pokemon={pokemon}/>
+    <>
+      <PokemonList pokemon={pokemon}/>
+      <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+      />
+    </>
   );
 }
 
